7470449b3e137fdff307fbe54d49992a7aea60f5
FelipeMovio <felipelsmovio@gmail.com>
2025-11-16 12:59:22 -0300

    update

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/security/SecurityConfig.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.felipemovio.TransacaoSimples.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 }
