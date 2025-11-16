fdc7656db036feda18aa801d027ad43c73dd8421
FelipeMovio <felipelsmovio@gmail.com>
2025-11-16 13:25:33 -0300

    update

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/security/SecurityConfig.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/security/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
     @Autowired
     private SecurityFilter securityFilter;
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
