7388199a34fa64f7d549751356ce732d0b5d2150
FelipeMovio <felipelsmovio@gmail.com>
2025-11-17 18:24:47 -0300

    update

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/AuthController.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/AuthController.java
@@ -1,9 +1,14 @@
 package com.felipemovio.TransacaoSimples.controller;
 
+import com.felipemovio.TransacaoSimples.DTO.request.RegisterRequestDTO;
+import com.felipemovio.TransacaoSimples.DTO.response.RegisterResponseDTO;
 import com.felipemovio.TransacaoSimples.security.TokenConfig;
 import com.felipemovio.TransacaoSimples.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +24,11 @@ public class AuthController {
 
     @Autowired
     private TokenConfig tokenConfig;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDTO> register(@RequestBody RegisterRequestDTO dto){
+        RegisterResponseDTO user = authService.register(dto);
+        return ResponseEntity.ok(user);
+    }
 
 }
