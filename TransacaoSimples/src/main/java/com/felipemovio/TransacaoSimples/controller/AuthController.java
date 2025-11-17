f0da635327599fecfff2e28b35c295c2f710754d
FelipeMovio <felipelsmovio@gmail.com>
2025-11-17 18:34:00 -0300

    controller de login

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/AuthController.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/AuthController.java
@@ -1,12 +1,18 @@
 package com.felipemovio.TransacaoSimples.controller;
 
+import com.felipemovio.TransacaoSimples.DTO.request.LoginRequestDTO;
 import com.felipemovio.TransacaoSimples.DTO.request.RegisterRequestDTO;
+import com.felipemovio.TransacaoSimples.DTO.response.LoginResponseDTO;
 import com.felipemovio.TransacaoSimples.DTO.response.RegisterResponseDTO;
+import com.felipemovio.TransacaoSimples.entity.Usuario;
 import com.felipemovio.TransacaoSimples.security.TokenConfig;
 import com.felipemovio.TransacaoSimples.services.AuthService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +32,19 @@ public class AuthController {
     private TokenConfig tokenConfig;
 
     @PostMapping("/register")
-    public ResponseEntity<RegisterResponseDTO> register(@RequestBody RegisterRequestDTO dto){
+    public ResponseEntity<RegisterResponseDTO> register(@RequestBody @Valid RegisterRequestDTO dto){
         RegisterResponseDTO user = authService.register(dto);
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO dto) {
+        Authentication auth = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getSenha())
+        );
+        Usuario user = (Usuario) auth.getPrincipal();
+        String token = tokenConfig.generateToken(user);
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
 }
