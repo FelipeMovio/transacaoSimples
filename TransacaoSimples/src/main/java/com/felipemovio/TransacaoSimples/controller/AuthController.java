64458f9e7127f3e22ac5de9174584451ad6f1977
FelipeMovio <felipelsmovio@gmail.com>
2025-11-17 18:19:34 -0300

    update

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/AuthController.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/AuthController.java
@@ -1,10 +1,23 @@
 package com.felipemovio.TransacaoSimples.controller;
 
+import com.felipemovio.TransacaoSimples.security.TokenConfig;
+import com.felipemovio.TransacaoSimples.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenConfig tokenConfig;
 
 }
