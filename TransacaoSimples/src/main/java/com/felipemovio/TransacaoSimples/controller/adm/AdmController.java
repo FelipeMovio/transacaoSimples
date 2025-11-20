22a1447aebd80c1aaac611ea9c4f49fda7f97c98
FelipeMovio <felipelsmovio@gmail.com>
2025-11-20 09:15:11 -0300

    controller de admin

---
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/adm/AdmController.java
@@ -0,0 +1,9 @@
+package com.felipemovio.TransacaoSimples.controller.adm;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdmController {
+}
