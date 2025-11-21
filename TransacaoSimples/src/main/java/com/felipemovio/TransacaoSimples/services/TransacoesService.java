29f7759fa49dd15278c8986cf42babb79e01e0ec
FelipeMovio <felipelsmovio@gmail.com>
2025-11-21 19:22:25 -0300

    create service para ver todas as transacoes feitas

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/services/TransacoesService.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/services/TransacoesService.java
@@ -1,12 +1,19 @@
 package com.felipemovio.TransacaoSimples.services;
 
+import com.felipemovio.TransacaoSimples.entity.Transacoes;
 import com.felipemovio.TransacaoSimples.repository.TransacoesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransacoesService {
 
     @Autowired
     private TransacoesRepository transacoesRepository;
+
+    public List<Transacoes> verTodas(){
+        return transacoesRepository.findAll();
+    }
 }
