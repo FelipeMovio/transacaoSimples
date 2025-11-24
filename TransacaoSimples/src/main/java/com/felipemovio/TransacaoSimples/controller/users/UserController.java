b05868b5adda824539d685b6e489d8dc36de3330
FelipeMovio <felipelsmovio@gmail.com>
2025-11-24 20:05:42 -0300

    controller de ver as transacoes do pagador feito

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/users/UserController.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/users/UserController.java
@@ -1,15 +1,21 @@
 package com.felipemovio.TransacaoSimples.controller.users;
 
+import com.felipemovio.TransacaoSimples.DTO.response.TransacoesResponseDTO;
 import com.felipemovio.TransacaoSimples.DTO.response.UsuarioDetalhesDTO;
+import com.felipemovio.TransacaoSimples.entity.Transacoes;
 import com.felipemovio.TransacaoSimples.entity.Usuario;
+import com.felipemovio.TransacaoSimples.mappers.TransacaoMapper;
 import com.felipemovio.TransacaoSimples.mappers.UsuarioMapper;
 import com.felipemovio.TransacaoSimples.services.TransacoesService;
 import com.felipemovio.TransacaoSimples.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -29,6 +35,15 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/transfer/pagador/{id}")
+    public ResponseEntity<List<TransacoesResponseDTO>> verTodasTransacoesDoUserPagador(@PathVariable Long id){
+       List <Transacoes> transacoes = transacoesService.verTodasPagadorById(id);
+       List <TransacoesResponseDTO> response = TransacaoMapper.toDTO(transacoes);
+        if (response.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(response);
+    }
 
 
 }
