249706d812d76bf162f0a5a3efffec700ec155af
FelipeMovio <felipelsmovio@gmail.com>
2025-11-24 19:16:53 -0300

    update

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/users/UserController.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/users/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/user")
@@ -22,10 +21,7 @@ public class UserController {
     @GetMapping("/me")
     public ResponseEntity<UsuarioDetalhesDTO> verUserLogado() {
         Usuario u = usuarioService.verConta();
-
         UsuarioDetalhesDTO response = UsuarioMapper.toDTO(u);
-
-
         return ResponseEntity.ok(response);
     }
 
