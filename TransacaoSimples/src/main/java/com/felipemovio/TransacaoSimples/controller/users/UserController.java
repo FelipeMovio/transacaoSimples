c7592c07d75888ad5848d2d81d06a07af5dfda46
FelipeMovio <felipelsmovio@gmail.com>
2025-11-24 20:00:22 -0300

    instanciando a classe no meu controller

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/users/UserController.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/controller/users/UserController.java
@@ -3,6 +3,7 @@ package com.felipemovio.TransacaoSimples.controller.users;
 import com.felipemovio.TransacaoSimples.DTO.response.UsuarioDetalhesDTO;
 import com.felipemovio.TransacaoSimples.entity.Usuario;
 import com.felipemovio.TransacaoSimples.mappers.UsuarioMapper;
+import com.felipemovio.TransacaoSimples.services.TransacoesService;
 import com.felipemovio.TransacaoSimples.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +19,16 @@ public class UserController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @Autowired
+    private TransacoesService transacoesService;
+
     @GetMapping("/me")
     public ResponseEntity<UsuarioDetalhesDTO> verUserLogado() {
         Usuario u = usuarioService.verConta();
         UsuarioDetalhesDTO response = UsuarioMapper.toDTO(u);
         return ResponseEntity.ok(response);
     }
+
+
 
 }
