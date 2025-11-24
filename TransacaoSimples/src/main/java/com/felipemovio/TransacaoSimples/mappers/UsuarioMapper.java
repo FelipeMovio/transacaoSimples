40550ffbd2cb7e64b6b9f180685a4afcfbe952fb
FelipeMovio <felipelsmovio@gmail.com>
2025-11-24 19:12:20 -0300

    update

--- TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/mappers/UsuarioMapper.java
+++ TransacaoSimples/src/main/java/com/felipemovio/TransacaoSimples/mappers/UsuarioMapper.java
@@ -1,4 +1,19 @@
 package com.felipemovio.TransacaoSimples.mappers;
 
+import com.felipemovio.TransacaoSimples.DTO.response.UsuarioDetalhesDTO;
+import com.felipemovio.TransacaoSimples.entity.Usuario;
+
+import java.util.stream.Collectors;
+
 public class UsuarioMapper {
+
+    public static UsuarioDetalhesDTO toDTO(Usuario u) {
+        return new UsuarioDetalhesDTO(
+                u.getId(),
+                u.getNomeCompleto(),
+                u.getEmail(),
+                u.getRoles().stream().map(Enum::name).collect(Collectors.toSet()),
+                u.getTipoUsuario().stream().map(Enum::name).collect(Collectors.toSet())
+        );
+    }
 }
